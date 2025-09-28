8707b9fc13aeacf60b75c87259d1a07406a0271f
dani filth <94713158+danifilth17@users.noreply.github.com>
2025-09-28 13:08:45 +0300

    Update QualityNEW.js

--- QualityNEW.js
+++ QualityNEW.js
@@ -11,8 +11,8 @@
         CACHE_REFRESH_THRESHOLD_MS: 12 * 60 * 60 * 1000, // 12 годин для фонового оновлення
         CACHE_KEY: 'lampa_quality_cache',
         JACRED_PROTOCOL: 'https://',
-        JACRED_URL: 'jacred.viewbox.dev',
-        JACRED_API_KEY: 'viewbox',
+        JACRED_URL: 'jacred.xyz',
+        JACRED_API_KEY: '',
         PROXY_LIST: [
             'http://api.allorigins.win/raw?url=',
             'http://cors.bwa.workers.dev/'
