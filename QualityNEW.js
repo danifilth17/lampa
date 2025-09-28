b925d5393b0195fbdb90a3b23cb89a3664499ad0
dani filth <94713158+danifilth17@users.noreply.github.com>
2025-09-28 17:37:54 +0300

    Update QualityNEW.js

--- QualityNEW.js
+++ QualityNEW.js
@@ -19,8 +19,9 @@
         ],
         PROXY_TIMEOUT_MS: 5000,
         SHOW_QUALITY_FOR_TV_SERIES: true,
-        MAX_PARALLEL_REQUESTS: 12, // Lite-черга: регульоване число паралельних пошуків (10-15 рекомендовано)
+        MAX_PARALLEL_REQUESTS: 10, // Lite-черга: регульоване число паралельних пошуків (10-15 рекомендовано)
         FULL_CARD_LABEL_BORDER_COLOR: '#8D3DA1',
+        FULL_CARD_LABEL_BACKGROUND_COLOR: 'rgba(132, 27, 208, 0.8)',
         FULL_CARD_LABEL_TEXT_COLOR: '#8D3DA1',
         FULL_CARD_LABEL_FONT_WEIGHT: 'bold',
         FULL_CARD_LABEL_FONT_SIZE: '1.2em',
